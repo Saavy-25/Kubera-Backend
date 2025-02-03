9417b9a192cb931f14ba7c5420a0f0118f6e4d67
Sarah Perez <70043414+sarahperez@users.noreply.github.com>
2025-02-03 14:54:16 -0500

    Connect to mongo (#14)
    * create .env and add to gitignore
    
    * connect to mongo, with logging
    
    * using MongoClient object instead
    
    * created mongo blueprint, basic CRUD example routes
    
    * gitignore pycache
    
    * ignore compiled Python bytecode files
    
    * updated readme with mongodb connection instructions

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
-import re
-from datetime import datetime
+import logging
 
 from flask import Flask
+from dotenv import load_dotenv
+from mongoClient.mongo_routes import mongo_bp 
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 
+# Set up logging
+logging.basicConfig(level=logging.DEBUG)
 
 @app.route("/")
 def home():
     return "Hello, Flask!"
 
-
-@app.route("/hello/<name>")
-def hello_there(name):
-    now = datetime.now()
-    formatted_now = now.strftime("%a, %d %b, %Y at %X")
-
-    content = "Hello there, " + name + "! It's " + formatted_now
-    return content
+# Register the Blueprint
+app.register_blueprint(mongo_bp, url_prefix='/mongo')
 
 if __name__ == "__main__":
     app.run(debug=True)
