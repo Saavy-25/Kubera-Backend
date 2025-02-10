3064ab1d09ab2fbe0cbd29f84b0fde178191e668
sophieruetschi <sophie.ruetschi@ufl.edu>
2025-02-10 14:04:41 -0500

    removing unused idx, replacing empty strings with None

--- Grocery/StoreProduct.py
+++ Grocery/StoreProduct.py
@@ -3,15 +3,15 @@ import json
 class StoreProduct:
     def __init__(self, product_name="", unit="", price="", date=None):
         #attribues same as in db
-        self.pk = "" # store#name#id(or UPC)
+        self.pk = "None" # store#name#id(or UPC)
         self.unit = unit #or quantity
-        self.generic_pk = ""
+        self.generic_pk = "None"
         
         #attributes differing from db schema
         self.price = price #list of timestamps/prices db, one value here for this object
         self.date = date
         self.product_name = product_name
-        self.generic_name = ""
+        self.generic_name = "None"
 
     def print(self):
         print("Product Name: ", self.product_name)
