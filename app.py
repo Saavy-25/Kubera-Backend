04b3d19274055c8708438e6ce481365c7aa178b0
sophieruetschi <sophie.ruetschi@ufl.edu>
2025-02-28 13:11:05 -0500

    Added Dockerfile, updated port on app.py

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ app.register_blueprint(auth_bp, url_prefix='/auth')
 app.register_blueprint(flutter_bp, url_prefix='/flutter')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
