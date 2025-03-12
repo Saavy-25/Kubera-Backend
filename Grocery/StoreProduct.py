ac0239361af195a3c37b11cf230c3102b3687173
Sophie Ruetschi <sophie.ruetschi@ufl.edu>
2025-03-12 09:48:52 -0400

    Post receipt db (#43)
    * adding posting receipt functionality
    
    * updating id field, adding comments
    
    * updates to ML integration, account for empty cache, update some field names
    
    * fixing json conventions, updating status in response
    
    * fix flipped date and price error

--- Grocery/StoreProduct.py
+++ Grocery/StoreProduct.py
@@ -1,24 +1,27 @@
 class StoreProduct:
-    def __init__(self, line_item, count, total_price, store_name=None, recent_prices=None, _id=None, store_product_name=None, generic_matches=None, generic_id=None) -> None:
+    def __init__(self, line_item, count, total_price, store_name=None, recent_prices=None, store_product_name=None, generic_matches=None) -> None:
         self.line_item = line_item
-        self.count = count
-        self.total_price = total_price
 
-        if not isinstance(count, int) or not isinstance(total_price, float):
-            raise ValueError("Count must be an integer. Total price must be a float.")
-        if count < 0:
-            raise ValueError(str("Count must be greater or equal to 1 to create a store product"))
-        if total_price < 0.0:
+        try:
+            self.count = int(count)
+            self.total_price = float(total_price)
+        except Exception:
+            raise TypeError("Valid count and price required to create a store product")
+        
+        if int(count) < 0:
+                raise ValueError(str("Count must be greater or equal to 1 to create a store product"))
+        if float(total_price) < 0.0:
             raise ValueError("Valid price required to create a store product")
 
         self.price_per_count = round( float(self.total_price)/ self.count, 2)
 
         self.store_name = store_name
         self.recent_prices = recent_prices
-        self.id = _id #_id in mongo
         self.store_product_name = store_product_name # expanded name
         self.generic_matches = generic_matches
-        self.generic_id = generic_id
+        self.date = None
+        self.id = None #_id in mongo
+        self.generic_id = None
 
     def print(self) -> None:
         '''print info stored in model'''
@@ -37,4 +40,29 @@ class StoreProduct:
             "pricePerCount": self.price_per_count,
             "storeProductId": self.id,
             "count": self.count
+        }
+    
+    def first_mongo_entry(self) -> dict:
+        '''return object as a dictionary for use for first insertion into mongo storeProducts collection'''
+        return {
+            "storeProductName": self.store_product_name,
+            "storeName": self.store_name,
+            "genericId": self.generic_id,
+            "recentPrices": [[self.price_per_count, self.date]]
+        }
+    
+    def get_map(self) -> dict:
+        '''return object as a dictionary for use for first insertion into mongo storeProducts collection'''
+        return {
+            "lineItem": self.line_item,
+            "count": self.count,
+            "totalPrice": self.total_price,
+            "pricePerCount": self.price_per_count,
+            "storeName": self.store_name,
+            "recentPrices": self.recent_prices,
+            "storeProductName": self.store_product_name,
+            "genericMatches": self.generic_matches,
+            "date": self.date,
+            "id": self.id,
+            "genericId": self.generic_id
         }
