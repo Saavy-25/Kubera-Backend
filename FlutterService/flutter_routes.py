5a120fb5ddf70a482e65f8ee82efdd562f7699de
Vy Vu <55815025+vudangbaovy@users.noreply.github.com>
2025-04-22 00:50:38 -0400

    get receipts route

--- FlutterService/flutter_routes.py
+++ FlutterService/flutter_routes.py
@@ -209,6 +209,36 @@ def post_receipt():
         e = ClientErrorMessage(message="We are having trouble saving your recipt, please try again.", detail=str(e))
         return jsonify(e.flutter_response()), 500
 
+@flutter_bp.route('/get_receipts', methods=['GET'])
+@login_required
+def get_receipts():
+    try:
+        if not current_user.username:
+            return jsonify({"error": "Missing user cookie"}), 400
+        
+        user_collection = mongoClient.get_collection(collection="users")
+        cur = user_collection.find_one({"username": current_user.username}, {'_id': 0, 'receiptIds': 1})
+        print(cur)
+        
+        receipts = []
+        receipt_collection = mongoClient.get_collection(collection="receipts")
+        for receipt_id in cur['receiptIds']:
+            receipt = receipt_collection.find_one({"_id": receipt_id}, {"_id": 0})
+
+            if not receipt:
+                # No client erorr message, just don't return a receipt that's not found
+                logging.error(f"Receipt with ID {receipt_id} not found")
+                continue
+            
+            for lineItem in receipt["lineItems"]:
+                lineItem["storeProductId"] = str(lineItem["storeProductId"]) if "storeProductId" in lineItem else None
+
+            logging.debug(f"Receipt: {receipt}")
+            receipts.append(receipt)
+        
+        return jsonify(receipts), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 @flutter_bp.route('/get_data', methods=['GET'])
 def receive_data():
