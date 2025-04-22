99736e11cd27671dee4acdb8caa834bc10994076
Vy Vu <55815025+vudangbaovy@users.noreply.github.com>
2025-04-22 02:39:02 -0400

    cast receiptIds from users document to BSON ObjectId

--- FlutterService/flutter_routes.py
+++ FlutterService/flutter_routes.py
@@ -223,14 +223,14 @@ def get_receipts():
         receipts = []
         receipt_collection = mongoClient.get_collection(collection="receipts")
         for receipt_id in cur['receiptIds']:
-            receipt = receipt_collection.find_one({"_id": receipt_id}, {"_id": 0})
+            receipt = receipt_collection.find_one({"_id": ObjectId(receipt_id)}, {"_id": 0})
 
             if not receipt:
                 # No client erorr message, just don't return a receipt that's not found
                 logging.error(f"Receipt with ID {receipt_id} not found")
                 continue
             
-            for lineItem in receipt["lineItems"]:
+            for lineItem in receipt["scannedLineItems"]:
                 lineItem["storeProductId"] = str(lineItem["storeProductId"]) if "storeProductId" in lineItem else None
 
             logging.debug(f"Receipt: {receipt}")
@@ -334,14 +334,14 @@ def get_storeProducts(generic_id):
         return f"An error occurred: {e}", 400
 
 # Currently not in use - just in case we want to refactor and only ping db for recentprices instead of the entire StoreProduct object
-@flutter_bp.route('/get_recentPrices/<product_id>', methods=['GET'])
+@flutter_bp.route('/get_productDetails/<product_id>', methods=['GET'])
 def get_productDetails(product_id):
     try:
         if not product_id:
             return jsonify({"error": "productId parameter is required"}), 400
         
         collection = mongoClient.get_collection(collection="storeProducts")
-        result = collection.find_one({"_id": ObjectId(product_id)}, {'recentPrices': 1, "_id": 0})
+        result = collection.find_one({"_id": ObjectId(product_id)}, {"_id": 0, "genericId": 0})
         if not result:
             return jsonify({"error": "No product found with the given ID"}), 404
 
